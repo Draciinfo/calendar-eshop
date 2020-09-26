57835dce4892810f475fafedffd38e9360f32473
Vojtech Oram <vojtech@oram.cz>
2020-09-26 21:50:26 +0200

    minor changes

--- calendareshop/shopping/management/commands/export_csv.py
+++ calendareshop/shopping/management/commands/export_csv.py
@@ -29,14 +29,14 @@ class Command(BaseCommand):
         with open('order_export.csv', 'w') as f:
             writer = csv.writer(f, delimiter=';', quoting=csv.QUOTE_MINIMAL)
             # write header
-            writer.writerow(['id', 'paid', 'email', 'shipping_address', 'order'])
+            writer.writerow(['id', 'paid', 'email', 'shipping_type', 'shipping_address', 'order', 'note'])
             # write rows
             for order in CustomOrder.objects.filter(status=CustomOrder.PAID, confirmed__year=datetime.date.today().year):
                 paid = order.is_paid()
                 email = order.email
                 shipping_address = full_shipping_address(order)
                 items = u"\n".join(map(lambda x: str(x).decode('utf8'), order.items.all()))
-                row = [order.id, paid, email, shipping_address, items]
+                row = [order.id, paid, email, order.shipping_type, shipping_address, items, order.notes]
                 row = [r.encode('utf8') if isinstance(r, basestring) else r for r in row]
                 writer.writerow(row)
 
